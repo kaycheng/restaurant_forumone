4e0d7dd8787762b20b317a6f3f359d57a3ce79af
KayCheng <kaycheng717@gmail.com>
2017-12-18 23:52:29 +0800

    add restaurant new and create actions and views for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -5,5 +5,26 @@ class Admin::RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
-  
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "Restaurant was created successfully."
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "Restaurant was failed to create."
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :tel, :address, :opening_hours, :description)
+  end
+
 end
