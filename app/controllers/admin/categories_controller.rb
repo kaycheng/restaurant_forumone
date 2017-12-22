dacbe8a1169f906f04a795b401258d2e0e04fe75
KayCheng <kaycheng717@gmail.com>
2017-12-22 14:29:21 +0800

    add form add errors messages on index page for creating new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,6 +4,25 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+    @category = Category.new
+
+    def create
+      @category = Category.new(category_params)
+      if @category.save
+        flash[:notice] = "Great!"
+        redirect_to admin_categories_path
+      else
+        @categories = Category.all
+        flash.now[:alert] = "Oops! We have some errors here!"
+        render :index
+      end
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
   
